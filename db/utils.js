908fe0f4f3d49171c6b8438da0f95009fc8b4211
Aman Kumar <urtheaman@gmail.com>
2023-05-19 13:32:01 +0530

    [++] created functional server

---
+++ db/utils.js
@@ -0,0 +1,40 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.username || "urtheaman"}:${
+  process.env.password || "7279856900"
+}@cluster0.ltcbygp.mongodb.net/?retryWrites=true&w=majority`;
+
+console.log(uri);
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+let todos;
+const connection = async () => {
+  await client.connect();
+  todos = client.db("db").collection("todos");
+};
+
+connection();
+
+const readAllDoc = async () => {
+  if (!todos) await connection();
+  const doc = await todos.find({}).toArray(function (err, result) {
+    if (err) throw err;
+  });
+  return doc;
+};
+
+const insertDoc = async (doc) => {
+  if (!todos) await connection();
+  const insertedId = await todos.insertOne(doc).then((res) => res.insertedId);
+  return insertedId;
+};
+
+module.exports = { readAllDoc, insertDoc };
